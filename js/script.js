1f580d6945eb7612d579b3b1736dde3ce994202c
chogukhee <3444855@hanmail.net>
2017-04-17 18:02:17 +0900

    add3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-
+$(function(){
 
   var sec_idx = [];
-  $('sec_wrap > div').each(function(){
+  $('.sec_wrap div').each(function(){
     sec_idx.push($(this).offset().top);
   })
 
-  console.log(sec_idx);
+  $(window).scroll(function(){
+    var scrW = $(this).scrollTop();
+
+    $('.sec_wrap div').each(function(i){
+      var hei = $(this).height() + sec_idx[i];
+
+      if(scrW > sec_idx[i] && scrW < hei){
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.nav li').eq(i).addClass('on').siblings().removeClass('on');
+      }
+
+    })
+  })
+
+  $('.nav li a').click(function(e){
+    e.preventDefault();
+    var sel_idx = $(this).parent().index();
+    $('body').stop().animate({
+      'scrollTop' : sec_idx[sel_idx]
+    })
+  })
+})
