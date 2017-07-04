96fa92504d941e5b5d114bd1d10d518c3ab99c1f
kimbomm <qhals0710@gmail.com>
2017-07-04 16:54:38 +0900

    commit 80%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  
+
   var hideCtl=0;
   var hide = setInterval(function(){
     if(hideCtl == 0){
@@ -10,4 +10,20 @@ $(function(){
       hideCtl = 0;
     }
   },700)
+
+  $('.cWrap').mousedown(function(){
+    $(this).css({
+      'background-color' : '#555',
+      'padding' : '10px 0 10px 40px'
+    }).find('span').css('color','#fff');
+    $('.cWrap ul').css('list-style','circle');
+  })
+  $('.cWrap').mouseup(function(){
+    $(this).css({
+      'background-color' : 'rgba(255,255,255,0)',
+      'padding' : '0'
+    }).find('span').css('color','#555');
+    $('.liTitle').css('color','#fff');
+    $('.cWrap ul').css('list-style','disc');
+  })
 })
